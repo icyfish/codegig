8730a994f0e859e9f7d42a3b58139638d464ca7c
Fish <bingyuzhang@trip.com>
2022-06-07 19:46:37 +0800

    1. connection and model

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const exHbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+// Database
+const db = require("./config/database");
+
+// test DB
+
+db.authenticate()
+  .then(() => console.log("Database connected successfully"))
+  .catch(err => console.log("Database connection failed: " + err));
+const app = express();
+
+app.get("/", (req, res) => res.send("INDEX"));
+
+// Gig routes
+app.use("/gigs", require("./routes/gigs"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
